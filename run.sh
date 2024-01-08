ae406b74cf1b0bfcdac5a059176e4b2786a7e3c1
Mac&Cheese Grater2.0 <rory.ahanan181@gmail.com>
2024-01-08 16:56:02 +0000

    Fixed relative paths

--- run.sh
+++ run.sh
@@ -6,18 +6,18 @@ sleep 3
 read -p 'Enter the IP which you want to stream on: ' ip_address
 
 echo 'Activating PYENV...'
-cd ../../myenv
+cd /bin/scripts/myenv/
 source bin/activate
 sleep 2
 
 echo 'Starting TS Generator...'
 sleep 2
-python3 ../../ts_generator.py
+python3 /bin/scripts/ts_generator.py
 sleep 2
 
 echo 'Starting Playlist Generator...'
 sleep 10
-python3 ../../playlist_generator.py
+python3 /bin/scripts/playlist_generator.py
 sleep 2
 
 echo 'Starting RTSP Server...'
@@ -27,7 +27,7 @@ sleep 2
 
 echo 'Starting Streamer with IP:', $ip_address
 sleep 3
-python3 ../../streamer.py $ip_address
+python3 /bin/scripts/streamer.py $ip_address
 sleep 2
 
 echo 'Services Started'
